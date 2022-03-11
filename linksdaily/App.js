498aeb11efff586bbf5ffae72be68d10a31098bf
tduncan14 <nbatre1@yahoo.com>
2022-03-11 10:57:42 -0800

    keep moving forward

--- linksdaily/App.js
+++ linksdaily/App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
 import {WebView} from 'react-native-webview';
 import Signup from './screensaver/Signup';
 import Signin from './screensaver/SignIn';
+
+
+ const Stack = createNativeStackNavigator();
 
 
 
@@ -11,12 +16,13 @@ import Signin from './screensaver/SignIn';
 export default function App() {
 
   return (
-  
-    <>
-      {/* <Signup /> */}
-      <Signin />
-      </>
 
+    <NavigationContainer>
+         <Stack.Navigator>
+            <Stack.Screen name="Signup" component={Signup}/>
+            <Stack.Screen name="Signin" component={Signin}/>
+         </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
